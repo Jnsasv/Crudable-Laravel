1582459d4ae34efe5131a8e89e50aad9794d92d7
juan carlos casillas campos <jns_asv@hotmail.com>
2022-09-13 11:48:44 -0500

    - test code in dashboard blade

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,13 +3,3 @@
             {{ __('Dashboard') }}
     </x-slot>
 </x-app-layout>
-
-@php
-    // dump(get_declared_classes())
-
-    $composer = require app_path() . '/vendor/autoload.php';
-    if (false === empty($composer)) {
-            $classes  = array_keys($composer->getClassMap());
-        }
-        dump($classes);
-@endphp
